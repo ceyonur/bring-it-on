ad365c718314620ab211662d44737d1c1d37dda6
Ceyhun Onur <ceyhunonur54@gmail.com>
2020-02-24 01:38:26 +0300

    use port env variable

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var app = require('./app');
 var mongoUtil = require( './util/mongo_util' );
+const port = process.env.PORT || 8080
 
 // connect to mongo server and listen
+
 mongoUtil.connect((err,client) => {
   if (err) {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
   }
   console.log("Successfully connected to the database");
-  app.listen(3000, () => {
+  app.listen(port, () => {
     console.log("Server is listening on port 3000");
   });
 });
