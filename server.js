8684572a369056751c17d464fc1b8ccbd72c8b4a
Ceyhun Onur <ceyhunonur54@gmail.com>
2020-02-24 01:45:44 +0300

    fix port log

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ mongoUtil.connect((err,client) => {
   }
   console.log("Successfully connected to the database");
   app.listen(port, () => {
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on port "  + port);
   });
 });
